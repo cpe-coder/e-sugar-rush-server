8f827e31bbfb41a36e8310b31ba9e286757ac354
Grian Gajila <griangajila2@gmail.com>
2025-01-22 00:05:20 +0800

    chore: creating post api for login screen

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const User = require("./model");
+
+require("dotenv").config();
 
 const mongoUrl = process.env.MONGODB_URI;
 
@@ -13,9 +16,34 @@ mongoose
 		console.log("Error connecting to database", err);
 	});
 
-app.get("/login", (req, res) => {
-	res.send({ status: "ok", message: "Hello World!" });
-	// make a login request from client using username and password if password and username is empty then create admin password and admin username
+app.post("/api/login", (req, res) => {
+	const { username, password } = req.body;
+
+	const findUser = User.findOne({ username, password })
+		.then((user) => {
+			if (user) {
+				res.send("Login successful");
+			} else {
+				res.status(401).send("Invalid credentials");
+			}
+		})
+		.catch((err) => {
+			console.log("Error logging in", err);
+			res.status(500).send("Internal server error");
+		});
+
+	if (findUser.length === 0) {
+		User.create({ username, password })
+			.then(() => {
+				res.send("User created successfully");
+			})
+			.catch((err) => {
+				console.log("Error creating user", err);
+				res.status(500).send("Internal server error");
+			});
+	} else {
+		findUser;
+	}
 });
 
 app.listen(3000, () => {
