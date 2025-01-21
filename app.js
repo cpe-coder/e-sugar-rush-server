f8feb4d0813da3e3772f2da80f6a6e9de15793c8
Grian Gajila <griangajila2@gmail.com>
2025-01-21 23:47:01 +0800

    wip

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const mongoUrl = process.env.MONGODB_URI;
+
+mongoose
+	.connect(mongoUrl)
+	.then(() => {
+		console.log("Connected to database");
+	})
+	.catch((err) => {
+		console.log("Error connecting to database", err);
+	});
 
 app.get("/login", (req, res) => {
 	res.send({ status: "ok", message: "Hello World!" });
 	// make a login request from client using username and password if password and username is empty then create admin password and admin username
-
-	if (req.query.username === "admin" && req.query.password === "admin") {
-		res.send({ status: "ok", message: "Hello World!" });
-	} else if (req.query.username === "" && req.query.password === "") {
-		res.send({
-			status: "error",
-			message: "Please enter username and password",
-		});
-	} else {
-		res.send({ status: "error", message: "Incorrect username or password" });
-	}
 });
 
 app.listen(3000, () => {
